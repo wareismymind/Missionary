3acb9c8d4351e8723e4c315c92fd5cbc885f1f9e
unknown <sl4gr00m@gmail.com>
2018-09-28 08:53:42 -0400

    Create FromStringConversion
    - Creates FromStringConversion type for automatic conversion from
    strings to supported types. Supported types are types that have either a
    string constructor or a static Parse method that returns an instance of
    the defining type.

---
+++ wimm.Missionary.UnitTests/FromStringConversionTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Xunit;
+
+namespace wimm.Missionary.UnitTests
+{
+    public class FromStringConversionTest
+    {
+        [Fact]
+        public void Supported_TypeIsNotSupported_ReturnsFalse()
+        {
+            Assert.False(FromStringConversion<object>.Supported());
+        }
+
+        [Fact]
+        public void Supported_TypeHasStringCtor_ReturnsTrue()
+        {
+            Assert.True(FromStringConversion<Uri>.Supported());
+        }
+
+        [Fact]
+        public void Supported_TypeHasParseMethod_ReturnsTrue()
+        {
+            Assert.True(FromStringConversion<int>.Supported());
+        }
+
+        [Fact]
+        public void Construct_TypeIsNotSupported_Throws()
+        {
+            var ex = Assert.Throws<NotSupportedException>(() =>
+            {
+                var _ = new FromStringConversion<object>();
+            });
+        }
+
+        [Fact]
+        public void Construct_TypeIsSupportedByConstructor_Constructs()
+        {
+            var _ = new FromStringConversion<Uri>();
+        }
+
+        [Fact]
+        public void Construct_TypeIsSupportedByParse_Constructs()
+        {
+            var _ = new FromStringConversion<int>();
+        }
+
+        [Fact]
+        public void Convert_CtorNullFrom_Throws()
+        {
+            var underTest = new FromStringConversion<Uri>();
+
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = underTest.Convert(null);
+            });
+        }
+
+        [Fact]
+        public void Convert_CtorInvalidArg_Throws()
+        {
+            var underTest = new FromStringConversion<Uri>();
+
+            var ex = Assert.Throws<InvalidCastException>(() =>
+            {
+                var _ = underTest.Convert("+++ not a uri +++");
+            });
+        }
+
+        [Fact]
+        public void Convert_CtorValidArg_ConvertsValue()
+        {
+            var from = "https://www.company.com";
+            var expected = new Uri(from);
+            var underTest = new FromStringConversion<Uri>();
+
+            var actual = underTest.Convert(from);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_ParseNullFrom_Throws()
+        {
+            var underTest = new FromStringConversion<Uri>();
+
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = underTest.Convert(null);
+            });
+        }
+
+        [Fact]
+        public void Convert_ParseInvalidArg_Throws()
+        {
+            var underTest = new FromStringConversion<Uri>();
+
+            var ex = Assert.Throws<InvalidCastException>(() =>
+            {
+                var _ = underTest.Convert("+++ not an int +++");
+            });
+        }
+
+        [Fact]
+        public void Convert_ParseValidArg_ConvertsValue()
+        {
+            var from = "42";
+            var expected = int.Parse(from);
+            var underTest = new FromStringConversion<int>();
+
+            var actual = underTest.Convert(from);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ wimm.Missionary/FromStringConversion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+
+namespace wimm.Missionary
+{
+    /// <summary>
+    /// A string conversion that's implemented by using either a constructor that accepts a single
+    /// string argument, or a public static Parse method to obtain the converted value. A type must
+    /// have at least of these methods available to be supported.
+    /// </summary>
+    /// <inheritDoc />
+    public class FromStringConversion<T> : IConversion<string, T>
+    {
+        private readonly DelegateConversion<string, T> _conversion;
+
+        /// <summary>
+        /// Indicates whether or not a type is supported.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <returns>
+        /// <c>true</c> is <typeparamref name="T"/> is supported, otherwise <c>false</c>.
+        /// </returns>
+        public static bool Supported()
+        {
+            return GetConversion() != null;
+        }
+
+        /// <summary>
+        /// Initializes a new new <see cref="FromStringConversion{T}"/>.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        /// <typeparamref name="T"/> has neither a constructor that accepts a single string
+        /// argument, nor a public static method called Parse that returns an instance of
+        /// <typeparamref name="T"/>.
+        /// </exception>
+        public FromStringConversion()
+        {
+            _conversion = new DelegateConversion<string, T>(GetConversion() ??
+                throw new NotSupportedException($"{nameof(T)} does not support automatic conversion from string."));
+        }
+
+        public T Convert(string from) => _conversion.Convert(from);
+
+        private static Func<string, T> GetConversion()
+        {
+            var constructor = typeof(T).GetConstructor(new[] { typeof(string) });
+            if (constructor != null)
+                return str => (T)constructor.Invoke(new[] { str });
+
+            var name = "Parse";
+            var bindingFlags = BindingFlags.Static | BindingFlags.Public;
+            var types = new[] { typeof(string) };
+            var parseMethod = typeof(T).GetMethod(name, bindingFlags, null, types, null);
+            if (parseMethod != null && parseMethod.ReturnType == typeof(T))
+                return str => (T)parseMethod.Invoke(null, new[] { str });
+
+            return null;
+        }
+    }
+}
